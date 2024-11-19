a47ce27ac2067d4589b978af5fa257102f725933
almaz <almaz@dreamersofday.co>
2024-11-19 17:35:40 +0100

    feature: init logger

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/kekaswork/grpc-auth/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	// TODO: init app configuration
 	cfg := config.MustLoad()
+	_ = cfg
 
-	fmt.Println(cfg)
 	// TODO: init logger
+	log := setupLogger(cfg.Env)
+	_ = log
+
 	// TODO: init application itself
 	// TODO: start grpc server
+}
+
+func setupLogger(env string) (log *slog.Logger) {
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return
 }
